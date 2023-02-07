62b8a5bd0052c09cec81caad547cc439b67385ba
Фатима Макабаева <m.f.a@list.ru>
2023-02-07 22:40:32 +0300

    Models update

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,7 +1,85 @@
 import datetime as dt
 
-from django.core.validators import MaxValueValidator
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth.tokens import default_token_generator
 from django.db import models
+
+from api_yamdb.settings import ADMIN, MODERATOR, USER
+from .validators import validate_username
+
+
+ROLE_CHOICES = [
+    (ADMIN, ADMIN),
+    (MODERATOR, MODERATOR),
+    (USER, USER)
+]
+
+
+class User(AbstractUser):
+    '''AbstractUser - разширенная модель пользователя(user)'''
+    username = models.CharField(
+        validators=(validate_username,),
+        max_length=150,
+        unique=True,
+        blank=False,
+        null=False,
+    )
+    email = models.EmailField(
+        max_length=254,
+        unique=True,
+        blank=False,
+        null=False
+    )
+    role = models.CharField(
+        'роль',
+        max_length=30,
+        choices=ROLE_CHOICES,
+        default=USER,
+        blank=True,
+    )
+    bio = models.TextField(
+        'биография',
+        blank=True,
+    )
+    first_name = models.CharField(
+        'имя',
+        max_length=150,
+        blank=True,
+    )
+    last_name = models.CharField(
+        'фамилия',
+        max_length=150,
+        blank=True,
+    )
+    confirmation_code = models.CharField(
+        'код подтверждения',
+        max_length=255,
+        null=True,
+        blank=False,
+        default='1111'
+    )
+
+    '''Декоратор @property возвращает из метода класса в атрибут класса'''
+    @property
+    def admin(self):
+        return self.role == ADMIN
+
+    @property
+    def moderator(self):
+        return self.role == MODERATOR
+
+    @property
+    def user(self):
+        return self.role == USER
+
+    class Meta:
+        ordering = ('id',)
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
 
 
 class Category(models.Model):
@@ -33,14 +111,10 @@ class Genre(models.Model):
 class Title(models.Model):
     """Модель для произведений."""
     name = models.CharField('Наименование произведения', max_length=256)
-    year = models.PositiveSmallIntegerField(
-        'Год выпуска', 
-        validators=[MaxValueValidator(
-            dt.datetime.now().year,
-            'Год не может быть больше текущего!'
-            )
-        ]
-    )
+    year = models.PositiveSmallIntegerField('Год выпуска', validators=[MaxValueValidator(
+        dt.datetime.now().year, 
+        'Год не может быть больше текущего!'
+    )])
     description = models.TextField('Описание',)
     genre = models.ManyToManyField(
         Genre,
@@ -68,5 +142,67 @@ class Title(models.Model):
 
 
 class GenreTitle(models.Model):
+    """Промежуточная таблица связи жанров и произведений"""
     title = models.ForeignKey(Title, on_delete=models.CASCADE)
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+
+class Review(models.Model):
+    """Модель для отзывов."""
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Пользователь')
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Отзыв')
+    text = models.TextField(
+        verbose_name='Текст')
+    pub_date = models.DateTimeField(
+        verbose_name='Дата добавления',
+        auto_now_add=True,
+        db_index=True)
+    score = models.PositiveSmallIntegerField(
+        verbose_name='Рейтинг',
+        validators=[
+            MinValueValidator(1, 'Введите число от 1 до 10'),
+            MaxValueValidator(10, 'Введите число от 1 до 10')
+        ]
+    )
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        ordering = ['pub_date']
+        constraints = [
+            models.UniqueConstraint(
+                fields=['title', 'author'],
+                name='unique_review')]
+
+
+class Comment(models.Model):
+    """Модель для комментарий."""
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Пользователь')
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Отзыв')
+    text = models.TextField(
+        verbose_name='Текст')
+    pub_date = models.DateTimeField(
+        verbose_name='Дата добавления',
+        auto_now_add=True,
+        db_index=True)
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ['pub_date']
