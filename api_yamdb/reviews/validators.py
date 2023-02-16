102caf34e15b8c94aeb50ea4a9c99ac0c487a64f
Taras Mikhaylov <jamahbully@gmail.com>
2023-02-16 20:42:15 +0500

    Правки

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -12,15 +12,9 @@ def validate_username(value):
         raise ValidationError(
             ('Имя не может быть - me'),
         )
-    if value != re.match(r'^[\w.@+-]+\Z', value):
-        print(re.sub(value, r'^[\w.@+-]+\Z', value))
-
-
-def validate_year(value):
-    if value > datetime.datetime.now().year:
-        raise ValidationError(
-            f'Указанный год {value} больше текущего'
-        )
+    if not re.match(r'^[\w.@+-]+\Z', value):
+        raise ValidationError((
+            f'{value} Имя пользователя содержит недопустимые символы!'))
 
 
 def validate_year(value):
